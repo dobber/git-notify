b8a4570abf6a12fd8c2bdf87f9e8ceabc05f2075
Ivan Dimitrov <zlobber@gmail.com>
2013-07-31 15:24:17 +0000

    Added help option Added dry-run option Some tuning and fixing

--- git-notify.sh
+++ git-notify.sh
@@ -3,37 +3,73 @@
 # Ivan Dimitrov https://github.com/dobber
 # for Ancient Media Ltd.
 
-repodir=/home/git/repositories/
-prettyf="%an - %s"
-email=your@email.addr
+mailer="/usr/bin/mail"
+repodir="/home/git/repositories/"
+prettyf="%an <%ae> - %s"
+from="From: root@${HOSTNAME}" # $HOSTNAME must be FQDN
+email="your@email.addr"
 template="Here is a list of changes in all repositories in out git"
 subject="Weekly list of git changes"
 period="1.week"
 
 ###### DO NOT EDIT BELOW THIS LINE ########
 index=0
+debug=0
 
 function generate_email {
-	if [ $index -eq 0 ] ; then
-		mail="$template
+        if [ $index -eq 0 ] ; then
+                mail="$template
 "
-		index=1
+                index=1
 
-	fi
-	mail="$mail
+        fi
+        mail="$mail
 
 $1
 $2
 =============================================="
 }
 
-for repo in `ls ${repodir}/` ; do
-	out=`git --git-dir=${repodir}${repo} log --since=${period} --pretty="${prettyf}"`
-	if [ -n "$out" ] ; then
-		generate_email "${repo}" "${out}"
-	fi
+function just_print {
+	echo "  ::      $1 ::"
+	echo "$2"
+	echo "=============================================="
+}
 
-	unset out
+case "$1" in
+	dry-run)
+		debug=1
+		;;
+	help)
+		echo $"Usage: $0 {help|dry-run|no-options}"
+		exit 0
+		;;
+	no-options)
+		echo "Actualy you can just run \`$0\` with no options and it will work too!"
+		debug=0;
+		;;
+	*)
+		debug=0
+esac
+
+if [ ! -d ${repodir} ] ; then
+        echo "Repo dir does not exist"
+        exit 0
+fi
+
+for repo in `ls ${repodir}/` ; do
+        out=`git --git-dir=${repodir}${repo} log --since=${period} --pretty="${prettyf}"`
+        if [ -n "$out" ] ; then
+                if [ $debug -eq 0 ] ; then
+                        generate_email "${repo}" "${out}"
+                else
+			just_print "${repo}" "${out}"
+                fi
+        fi
+
+        unset out
 done
 
-echo "${mail}" | mail -s "${subject}" "${email}"
+if [ $debug -eq 0 ] ; then
+        echo "${mail}" | ${mailer} -s "${subject}" "${email}"
+fi
